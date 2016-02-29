c5098272e435e66ba1da312c90062b8914e40cdf
Matt Heller <mjheller89@gmail.com>
2016-02-28 21:48:43 -0600

    scaffolded with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularCommerceApp
+ * @description
+ * # angularCommerceApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularCommerceApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCommerceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularCommerceApp
+ */
+angular.module('angularCommerceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
